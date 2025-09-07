747dffa21f4a59932d4f5ea5b6bbb8796ae3b890
ahmedmalik07 <ahmed.usman7615@gmail.com>
2025-09-07 11:55:50 +0500

    reqs added

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="medical_chatbot",
     version="0.1.0",
-    author="Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    author="Ahmed Malik",
+    author_email="ahmedmalik7615@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
