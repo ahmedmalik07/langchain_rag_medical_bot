505f098fd1ca48c26aded838e7d67a841aeeae98
ahmedmalik07 <ahmed.usman7615@gmail.com>
2025-09-14 10:39:35 +0500

    Initial commit

--- app.py
+++ app.py
@@ -9,18 +9,15 @@ from dotenv import load_dotenv
 from src.prompt import *
 import os
 
-
 app = Flask(__name__)
-
 
 load_dotenv()
 
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-
 
 embeddings = download_hugging_face_embeddings()
 
@@ -31,12 +28,9 @@ docsearch = PineconeVectorStore.from_existing_index(
     embedding=embeddings
 )
 
-
-
-
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
 
-chatModel = ChatOpenAI(model="gpt-4o")
+chatModel = ChatOpenAI(model="gpt-4o-mini")
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
@@ -48,11 +42,9 @@ question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
 
-
 @app.route("/")
 def index():
     return render_template('chat.html')
-
 
 
 @app.route("/get", methods=["GET", "POST"])
@@ -65,6 +57,5 @@ def chat():
     return str(response["answer"])
 
 
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
